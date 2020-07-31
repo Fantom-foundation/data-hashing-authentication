205833beee170e5a14f9f2ec6c405ffbf7aa1834
Jiri Malek <jirka.malek@gmail.com>
2020-07-31 09:41:00 +0200

    Add input structure experimental support.

--- contracts/DataHashAuth.sol
+++ contracts/DataHashAuth.sol
@@ -1,5 +1,6 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.5.0;
+pragma experimental ABIEncoderV2;
 
 // DataHashAuth implements contract for registering authentic
 // products hash inside the ledger by hashing their unique attributes
@@ -17,7 +18,18 @@ contract DataHashAuth {
         uint64 expiration; // the UTC timestamp of the expiration date
         uint64 added; // the time stamp of the product record creation
         uint64 updated; // the time stamp of the last product update
-        unit64 invalidated; // the time stamp of the product invalidation
+        uint64 invalidated; // the time stamp of the product invalidation
+    }
+
+    // InputProduct defines a structure for product input.
+    struct InputProduct {
+        uint256 pin;
+        string name;
+        string producer;
+        string batchNo;
+        string barcode;
+        uint64 produced;
+        uint64 expiration;
     }
 
     // manager is the account allowed to register new authentic products
@@ -52,22 +64,44 @@ contract DataHashAuth {
         _scanners[msg.sender] = true;
     }
 
-    // setProduct adds new authentic product data set, or updates
+    // register adds/updates a batch of products information
+    // from an authorized scanner address in the contract.
+    function register(InputProduct[] calldata _inProducts) external {
+        // make sure this is autenticated access
+        require(_scanners[msg.sender], "access restricted");
+
+        // loop all incoming data records and process each
+        // into the contract
+        for (uint i = 0; i < _inProducts.length; i++) {
+            // process the product in the contract
+            _store(
+                _inProducts[i].pin,
+                _inProducts[i].name,
+                _inProducts[i].producer,
+                _inProducts[i].batchNo,
+                _inProducts[i].barcode,
+                _inProducts[i].produced,
+                _inProducts[i].expiration
+            );
+        }
+    }
+
+    // _store adds new authentic product data set, or updates
     // an existing product set in the contract. The PIN is used
     // as an unique identifier of the product, hash is generated
     // to support the product details validation on subsequent checks.
     // Only authenticated scanners are allowed to perform this function.
-    function setProduct(
+    function _store(
         uint256 _pin,
-        string calldata _name,
-        string calldata _producerName,
-        string calldata _batchNo,
-        string calldata _barcodeNo,
+        string memory _name,
+        string memory _producerName,
+        string memory _batchNo,
+        string memory _barcodeNo,
         uint64 _productionDate,
         uint64 _expiryDate
-    ) external returns (bytes32) {
-        // make sure this is autenticated access
-        require(_scanners[msg.sender], "access restricted");
+    ) private {
+        // the product PIN is expected to be unique
+        bool isNew = (_products[_pin].added == 0);
 
         // calculate the hash
         bytes32 _hash = _hashProduct(
@@ -79,11 +113,10 @@ contract DataHashAuth {
             _expiryDate,
             _productionDate);
 
-        // the product PIN is expected to be unique
-        bool isNew = (_products[_pin].added == 0);
-
-        // enlist the product in the contract
+        // enlist the product in the contract (aloc the storage for it)
         TProduct storage inProduct = _products[_pin];
+
+        // update the product data
         inProduct.productHash = _hash;
         inProduct.name = _name;
         inProduct.producer = _producerName;
@@ -93,41 +126,27 @@ contract DataHashAuth {
         inProduct.expiration = _expiryDate;
 
         // set the product timestamp record to recognize the action
-        // emit the product event based on set status
+        // and emit the appropriate product event
         if (isNew) {
             // the product didn't exist before and so it's a new one
-            inProduct.added = now;
-            emit ProductAdded(_pin, _hash, now);
+            inProduct.added = uint64(now);
+            emit ProductAdded(_pin, _hash, uint64(now));
         } else {
             // the product existed before and so it's an update
-            inProduct.updated = now;
-            emit ProductUpdated(_pin, _hash, now);
+            inProduct.updated = uint64(now);
+            emit ProductUpdated(_pin, _hash, uint64(now));
         }
-
-        // return the generated hash value
-        return _hash;
     }
 
-    // productHashByPin returns a hash of a product identified by the PIN
-    // if the product is recognized by the contract, or zero hash for unknown
-    // products.
-    function productHashByPin(uint256 _pin) external view returns (bytes32) {
-        // invalidated products are skipped on this check
-        if (_products[_pin].invalidated > 0) {
-            return 0x0;
-        }
-        return _products[_pin].productHash;
-    }
-
-    // authProduct validates product authenticity for the given product data set
+    // auth validates product authenticity for the given product data set
     // using an internal authentic products list. Anybody can authenticate
     // products using this function, no access restrictions are applied.
-    function authProduct(
+    function auth(
         uint256 _pin,
-        string calldata _name,
-        string calldata _producerName,
-        string calldata _batchNo,
-        string calldata _barcodeNo,
+        string memory _name,
+        string memory _producerName,
+        string memory _batchNo,
+        string memory _barcodeNo,
         uint64 _productionDate,
         uint64 _expiryDate
     ) public view returns (bytes32, bool) {
@@ -146,6 +165,17 @@ contract DataHashAuth {
         return (_hash, 0 == _products[_pin].invalidated && _hash == _products[_pin].productHash);
     }
 
+    // hashByPin returns a hash of a product identified by the PIN.
+    // If the product is not recognized by the contract,
+    // zero hash is returned.
+    function hashByPin(uint256 _pin) external view returns (bytes32) {
+        // invalidated products are skipped on this check
+        if (_products[_pin].invalidated > 0) {
+            return 0x0;
+        }
+        return _products[_pin].productHash;
+    }
+
     // invalidate sets the product status to invalid.
     function invalidate(uint256 _pin) external {
         // make sure this is autenticated access
@@ -155,20 +185,20 @@ contract DataHashAuth {
         require(_products[_pin].added > 0, "unknown product");
 
         // make the change
-        _products[_pin].invalidated = now;
+        _products[_pin].invalidated = uint64(now);
 
         // emit the event
-        emit ProductInvalidated(_pin, now);
+        emit ProductInvalidated(_pin, uint64(now));
     }
 
     // _hash calculates the hash of the product used for both
     // the product registration and validation procedures.
     function _hashProduct(
         uint256 _pin,
-        string calldata _name,
-        string calldata _producerName,
-        string calldata _batchNo,
-        string calldata _barcodeNo,
+        string memory _name,
+        string memory _producerName,
+        string memory _batchNo,
+        string memory _barcodeNo,
         uint64 _productionDate,
         uint64 _expiryDate
     ) internal pure returns (bytes32) {
